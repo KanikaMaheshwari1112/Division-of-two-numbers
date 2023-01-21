b950fd81f0f6ced39c3f64581ec5d48c75934fcb
Kanika Maheshwari <22ds1000263@student.onlinedegree.iitm.ac.in>
2023-01-21 17:15:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,10 @@ st.header('Input two numbers')
 
 a= st.number_input('Enter first number')
 b= st.number_input('Enter second number')
-Division= a/b 
+if b=0.0:
+  print('Zero error')
+else:
+  Division= a/b 
 
 st.write('The Division of given numbers is', Division)
 st.write('By Kanika Maheshwari')
